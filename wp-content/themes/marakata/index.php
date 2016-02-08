b610e52f4a52d3792f9070b6bacba3a0a22738ef
Ivan Avraamov <ivan.avraamov@gmail.com>
2016-02-08 15:59:18 +0300

    marakata-23

--- wp-content/themes/marakata/index.php
+++ wp-content/themes/marakata/index.php
@@ -26,8 +26,8 @@
         <section class="section section--menu" id="Adrian">
           <nav class="menu menu--adrian">
             <ul class="menu__list">
-              <li class="menu__item"><a href="#" class="menu__link"><span class="menu__helper">MARAKATA</span></a></li>
-              <li class="menu__item menu__item--current"><a href="#" class="menu__link"><span class="menu__helper">О ПРОЕКТЕ</span></a></li>
+              <li class="menu__item menu__item--current"><a href="#" class="menu__link"><span class="menu__helper">MARAKATA</span></a></li>
+              <li class="menu__item"><a href="#" class="menu__link"><span class="menu__helper">О ПРОЕКТЕ</span></a></li>
               <li class="menu__item"><a href="#" class="menu__link"><span class="menu__helper">ПРОТОКОЛ</span></a></li>
               <li class="menu__item"><a href="#" class="menu__link"><span class="menu__helper">НОВОСТИ</span></a></li>
             </ul>
