f7d5627bac2d56abde7278320fb84f65dc23528a
Ivan Avraamov <ivan.avraamov@gmail.com>
2016-02-23 14:30:38 +0300

    marakata-48

--- wp-content/themes/marakata/index.php
+++ wp-content/themes/marakata/index.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
-  <!-- <link rel="stylesheet" href="<?php //bloginfo('template_url'); ?>/css/animate.css" /> -->
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/animate.css" />
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/normalize.css" />
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/demo.css" />
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/component.css" />
