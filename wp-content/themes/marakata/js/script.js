78020a898a392a4a63f60a45d9af23b59da48725
Ivan Avraamov <avraamov@tadatuta.com>
2016-03-03 11:40:05 +0300

    mara-14

--- wp-content/themes/marakata/js/script.js
+++ wp-content/themes/marakata/js/script.js
@@ -117,7 +117,7 @@ jQuery(function() {
     });
     jQuery('.marakata_sim').on('click', function(event) {
       curTrY = parseFloat(jQuery(this).css('backgroundPositionY'));
-      curTrY = curTrY+218;
+      curTrY = curTrY-218;
       jQuery(this).css('backgroundPositionY', curTrY+'px');
     });
 });
