7df3d9c071bde8ab90ab1ec358143480c32e88c7
Ivan Avraamov <avraamov@tadatuta.com>
2016-03-10 13:22:05 +0300

    mara-89

--- wp-content/themes/marakata/js/script.js
+++ wp-content/themes/marakata/js/script.js
@@ -233,6 +233,18 @@ jQuery(function() {
       }
       if (counter == 9 && global_counter <= 5 ) {
         counter = -1;
+        elems_obj = {
+              0: 0,
+              1: 0,
+              2: 0,
+              3: 0,
+              4: 0,
+              5: 0,
+              6: 0,
+              7: 0,
+              8: 0,
+              9: 0,
+            };
         global_counter += 1;
       }
     });
