a7b781a77e135e8d4eeb109bcb68da4b01bbe597
Ivan Avraamov <avraamov@tadatuta.com>
2016-03-15 16:26:34 +0300

    prot fix-5

--- wp-content/themes/marakata/js/script.js
+++ wp-content/themes/marakata/js/script.js
@@ -233,7 +233,8 @@ jQuery(function() {
         curTrY = parseFloat(jQuery(this).css('backgroundPositionY'));
         curTrY = curTrY-(212.5*max_time);
         console.log(jQuery(this));
-        jQuery(this).css('backgroundPositionY', curTrY+'px').removeClass('marakata_sim-active, marakata_sim_from');
+        jQuery(this).css('backgroundPositionY', curTrY+'px');
+        jQuery(this).removeClass('marakata_sim-active, marakata_sim_from');
         counter = -1;
         global_counter += 1;
         if (global_counter >= 5) {
