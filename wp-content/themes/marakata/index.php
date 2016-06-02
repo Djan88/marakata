c0cf1bd62698c96a8ce4362db1ed146dcc4bc69c
Ivan Avraamov <ivan.avraamov@gmail.com>
2016-06-02 13:47:44 +0300

    user roles

--- wp-content/themes/marakata/index.php
+++ wp-content/themes/marakata/index.php
@@ -21,22 +21,69 @@
   <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script><![endif]-->
 </head>
 <body>
-  <div class="container main-header">
-    <div class="row">
-      <div class="col-md-12">
-        <section class="section section--menu" id="Adrian">
-          <nav class="menu menu--adrian">
-            <ul class="menu__list">
-              <li class="menu__item menu__item--current"><a href="#" class="menu__link menu-main"><span class="menu__helper">MARAKATA</span></a></li>
-              <li class="menu__item"><a href="#" class="menu__link menu-about"><span class="menu__helper">О ПРОЕКТЕ</span></a></li>
-              <li class="menu__item"><a href="#" class="menu__link menu-proto"><span class="menu__helper">ПРОТОКОЛ</span></a></li>
-              <li class="menu__item"><a href="#" class="menu__link menu-news"><span class="menu__helper">НОВОСТИ</span></a></li>
-            </ul>
-          </nav>
-        </section>
+  <?php if(is_user_logged_in()){ ?>
+      <!-- Если зашел подписчик -->
+      <?php if(current_user_can('contributor') || current_user_can('administrator')) { ?>
+         <div class="container main-header">
+           <div class="row">
+             <div class="col-md-12">
+               <section class="section section--menu" id="Adrian">
+                 <nav class="menu menu--adrian">
+                   <ul class="menu__list">
+                     <li class="menu__item menu__item--current"><a href="#" class="menu__link menu-main"><span class="menu__helper">MARAKATA</span></a></li>
+                     <li class="menu__item"><a href="#" class="menu__link menu-about"><span class="menu__helper">О ПРОЕКТЕ</span></a></li>
+                     <li class="menu__item"><a href="#" class="menu__link menu-proto"><span class="menu__helper">ПРОТОКОЛ</span></a></li>
+                     <li class="menu__item"><a href="#" class="menu__link menu-news"><span class="menu__helper">НОВОСТИ</span></a></li>
+                   </ul>
+                 </nav>
+               </section>
+             </div>
+           </div>
+         </div>    
+      <?php } ?>
+  
+  <?php } else { ?>
+      <div class="login__form">
+        <div class="container main-header">
+          <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+              <form name="loginform" id="loginform" action="<?php echo esc_url( site_url( 'wp-login.php', 'login_post' ) ); ?>" method="post">
+                  <p>
+                      <label for="user_login"><?php _e('Username') ?><br />
+                      <input type="text" name="log" id="user_login" class="input" value="<?php echo esc_attr($user_login); ?>" size="20" /></label>
+                  </p>
+                  <p>
+                      <label for="user_pass"><?php _e('Password') ?><br />
+                      <input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+                  </p>
+                  <?php
+                  /**
+                   * Fires following the 'Password' field in the login form.
+                   *
+                   * @since 2.1.0
+                   */
+                  do_action( 'login_form' );
+                  ?>
+                  <p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever" <?php checked( $rememberme ); ?> /> <?php esc_attr_e('Remember Me'); ?></label></p>
+                  <p class="note_small">Для получения сертификата пользователя необходимо пройти очное либо дистантное обучение. <a class="bablosadres" href="mailto:info@bablosstudio.ru" style="color: #fff;">info@bablosstudio.ru</a></p>
+                  <p class="submit">
+                      <input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="<?php esc_attr_e('Log In'); ?>" />
+              <?php   if ( $interim_login ) { ?>
+                      <input type="hidden" name="interim-login" value="1" />
+              <?php   } else { ?>
+                      <input type="hidden" name="redirect_to" value="<?php echo esc_attr($redirect_to); ?>" />
+              <?php   } ?>
+              <?php   if ( $customize_login ) : ?>
+                      <input type="hidden" name="customize-login" value="1" />
+              <?php   endif; ?>
+                      <input type="hidden" name="testcookie" value="1" />
+                  </p>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+  <?php } ?>
   <canvas class="content-block content-block_main"></canvas>
   <div class="container content-block content-block_about hidden">
     <div class="row">
