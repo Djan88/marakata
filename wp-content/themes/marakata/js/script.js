0115c0d9f16a5b87915b30274916de0518c31e85
Ivan Avraamov <ivan.avraamov@gmail.com>
2016-10-05 17:31:29 +0300

    mrakata_game_17

--- wp-content/themes/marakata/js/script.js
+++ wp-content/themes/marakata/js/script.js
@@ -112,7 +112,7 @@ jQuery(function() {
       } else {
         scroll_val = 123;
       }
-      scroll_game = 400;
+      scroll_game = 368;
     }
     cur_window_width();
     jQuery(window).on('resize', function(event) {
