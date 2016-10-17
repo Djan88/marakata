898a5c835f66a54e45b7f55930be8674b88d60f6
Ivan Avraamov <ivan.avraamov@gmail.com>
2016-10-17 10:03:58 +0300

    numbers

--- wp-content/themes/marakata/index.php
+++ wp-content/themes/marakata/index.php
@@ -251,21 +251,13 @@
     <div class="row">
       <div class="col-md-12 no_padding_wrap content">
         <div class="row">
-          <h2 class="col-md-12 marakata_heading">Исходные данные</h2>
+          <h2 class="col-md-12 marakata_heading"><div class="game_numbers"></div><button type="button" class="btn btn-default btn-small btn_game">Начать с начала</button></h2>
           <div class="col-md-4 col-sm-6 col-xs-9 marakata_sim-wrap marakata_game"><div class="marakata_sim marakata_sim_g marakata_sim-0 marakata_sim_game"></div></div>
-          <!-- <div class="col-md-4 col-xs-4 marakata_sim-wrap marakata_game hidden"><div class="marakata_sim marakata_sim_g marakata_sim-1 marakata_sim_game"></div></div>
-          <div class="col-md-4 col-xs-4 marakata_sim-wrap marakata_game hidden"><div class="marakata_sim marakata_sim_g marakata_sim-2 marakata_sim_game"></div></div>
-          <div class="col-md-4 col-xs-4 marakata_sim-wrap marakata_game hidden"><div class="marakata_sim marakata_sim_g marakata_sim-3 marakata_sim_game"></div></div>
-          <div class="col-md-4 col-xs-4 marakata_sim-wrap marakata_game hidden"><div class="marakata_sim marakata_sim_g marakata_sim-4 marakata_sim_game"></div></div>
-          <div class="col-md-4 col-xs-4 marakata_sim-wrap marakata_game hidden"><div class="marakata_sim marakata_sim_g marakata_sim-5 marakata_sim_game"></div></div>
-          <div class="col-md-4 col-xs-4 marakata_sim-wrap marakata_game hidden"><div class="marakata_sim marakata_sim_g marakata_sim-6 marakata_sim_game"></div></div>
-          <div class="col-md-4 col-xs-4 marakata_sim-wrap marakata_game hidden"><div class="marakata_sim marakata_sim_g marakata_sim-7 marakata_sim_game"></div></div>
-          <div class="col-md-4 col-xs-4 marakata_sim-wrap marakata_game hidden"><div class="marakata_sim marakata_sim_g marakata_sim-8 marakata_sim_game"></div></div> -->
         </div>
       </div>
       <div class="col-md-12">
         <div class="row" style="text-align: center;">
-          <button type="button" class="btn btn-default btn-lg btn_reset">Начать с начала</button>
+          <button type="button" class="btn btn-default btn-lg btn_game_reset">Начать с начала</button>
         </div>
       </div>
     </div>
