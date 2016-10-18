46e4e6d72d632607e116284ca21580270f735dbd
Ivan Avraamov <avraamov@tadatuta.com>
2016-10-18 16:23:33 +0300

    game-35

--- wp-content/themes/marakata/functions.php
+++ wp-content/themes/marakata/functions.php
@@ -7,4 +7,12 @@ if(!current_user_can('manage_options')){
 add_action('admin_init','users_redirect');
 add_filter('login_redirect', 'users_redirect');
 }
+function is_user_role( $role, $user_id = null ) {
+  $user = is_numeric( $user_id ) ? get_userdata( $user_id ) : wp_get_current_user();
+
+  if( ! $user )
+    return false;
+
+  return in_array( $role, (array) $user->roles );
+}
 ?>
