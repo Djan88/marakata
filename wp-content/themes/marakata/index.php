2d93b4a1e22c2a99b7f5cd88ac39b74e008a070f
Ivan Avraamov <avraamov@tadatuta.com>
2016-10-19 16:38:45 +0300

    game-39

--- wp-content/themes/marakata/index.php
+++ wp-content/themes/marakata/index.php
@@ -23,7 +23,7 @@
 <body>
   <?php if(is_user_logged_in()){ ?>
       <!-- Если зашел участник или автор -->
-      <?php if(is_user_role('contributor') || is_user_role('administrator') || is_user_role('autor')) { ?>
+      <?php if(is_user_role('contributor') || is_user_role('administrator') || is_user_role('author')) { ?>
          <div class="container main-header">
            <div class="row">
              <div class="col-md-12">
@@ -33,7 +33,7 @@
                      <li class="menu__item menu__item--current"><a href="#" class="menu__link menu-main"><span class="menu__helper">MARAKATA</span></a></li>
                      <li class="menu__item"><a href="#" class="menu__link menu-about"><span class="menu__helper">О ПРОЕКТЕ</span></a></li>
                      <li class="menu__item"><a href="#" class="menu__link menu-proto"><span class="menu__helper">ПРОТОКОЛ</span></a></li>
-                     <?php if(is_user_role('administrator') || is_user_role('autor')) { ?>
+                     <?php if(is_user_role('administrator') || is_user_role('author')) { ?>
                      <li class="menu__item"><a href="#" class="menu__link menu-game"><span class="menu__helper">ИГРА</span></a></li>
                      <?php } ?>
                      <!-- <li class="menu__item"><a href="#" class="menu__link menu-news"><span class="menu__helper">НОВОСТИ</span></a></li> -->
