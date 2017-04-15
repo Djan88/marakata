57d00a20e980bff9e03500f900032bbffdf4999d
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-04-15 10:11:01 +0300

    status

--- wp-content/themes/marakata/index.php
+++ wp-content/themes/marakata/index.php
@@ -68,46 +68,7 @@
       <?php } ?>
   
   <?php } else { ?>
-      <div class="login__form">
-        <div class="container main-header">
-          <div class="row">
-            <div class="col-md-6 col-md-offset-3">
-              <form name="loginform" id="loginform" action="<?php echo esc_url( site_url( 'wp-login.php', 'login_post' ) ); ?>" method="post">
-                  <p>
-                      <label for="user_login"><?php _e('Username') ?><br />
-                      <input type="text" name="log" id="user_login" class="input" value="<?php echo esc_attr($user_login); ?>" size="20" /></label>
-                  </p>
-                  <p>
-                      <label for="user_pass"><?php _e('Password') ?><br />
-                      <input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
-                  </p>
-                  <?php
-                  /**
-                   * Fires following the 'Password' field in the login form.
-                   *
-                   * @since 2.1.0
-                   */
-                  do_action( 'login_form' );
-                  ?>
-                  <p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever" <?php checked( $rememberme ); ?> /> <?php esc_attr_e('Remember Me'); ?></label></p>
-                  <p class="submit">
-                      <input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="<?php esc_attr_e('Log In'); ?>" />
-              <?php   if ( $interim_login ) { ?>
-                      <input type="hidden" name="interim-login" value="1" />
-              <?php   } else { ?>
-                      <input type="hidden" name="redirect_to" value="/" />
-              <?php   } ?>
-              <?php   if ( $customize_login ) : ?>
-                      <input type="hidden" name="customize-login" value="1" />
-              <?php   endif; ?>
-                      <input type="hidden" name="testcookie" value="1" />
-                  </p>
-                  <p class="note_small">Вопросы связанные с получением доступа на проект "MARAKATA" можно задать на <a class="bablosadres" href="mailto:info@bablosstudio.ru" style="color: #fff;">info@bablosstudio.ru</a></p>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>test</div>
   <?php } ?>
   <canvas class="content-block content-block_main"></canvas>
   <div class="container content-block content-block_about hidden">
