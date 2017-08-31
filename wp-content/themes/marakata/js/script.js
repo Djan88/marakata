8ce8a6ffafde0249025d15e94992bf72e18ff53c
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-08-31 15:05:14 +0300

    screens w-32

--- wp-content/themes/marakata/js/script.js
+++ wp-content/themes/marakata/js/script.js
@@ -41,7 +41,7 @@ jQuery(function() {
             jQuery(".marakata_sim").css('height', scroll_val+'px');
             jQuery(".marakata_dot").css('height', scroll_val+'px');
         } else {
-            scroll_val =  block_w * 1.198381424059719;
+            scroll_val =  block_w * 1.199377930902479;
             jQuery(".marakata_sim").css('height', scroll_val+'px');
             jQuery(".marakata_dot").css('height', scroll_val+'px');
         }
