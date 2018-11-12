5f4b0bbca300ac688e3be87b7bcf9c78c255573b
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-11-12 20:00:53 +0300

    marakata_new-42

--- wp-content/themes/marakata_new/js/main.js
+++ wp-content/themes/marakata_new/js/main.js
@@ -1,6 +1,34 @@
 "use strict";
 var $ = jQuery;
 
+//Form Stylization
+function formStylization(){
+	var radio    = 'input[type="radio"]',
+			checkbox = 'input[type="checkbox"]';
+
+	$(radio).wrap('<div class="new-radio"></div>').closest('.new-radio').append('<span></span>');
+	$(checkbox).wrap('<div class="new-checkbox"></div>').closest('.new-checkbox').append('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 13 11" enable-background="new 0 0 13 11" xml:space="preserve"><polyline points="0.2,4.6 1,4 5,9 12,0 12.8,0.6 5.8,9.6 5,10.6 4.2,9.6 0.2,4.6 "/></svg>');
+	$(checkbox + ':checked').parent('.new-checkbox').addClass('checked');
+	$(radio + ':checked').parent('.new-radio').addClass('checked');
+	$(checkbox + ':disabled').closest('.checkbox').addClass('disabled');
+	$(radio + ':disabled').closest('.radio').addClass('disabled');
+
+	$('body').on('click', function(){
+		$(radio).parent('.new-radio').removeClass('checked');
+		$(radio + ':checked').parent('.new-radio').addClass('checked');
+		$(checkbox).parent('.new-checkbox').removeClass('checked');
+		$(checkbox + ':checked').parent('.new-checkbox').addClass('checked');
+		$(radio).closest('.radio').removeClass('disabled');
+		$(checkbox).closest('.checkbox').removeClass('disabled');
+		$(radio + ':disabled').closest('.radio').addClass('disabled');
+		$(checkbox + ':disabled').closest('.checkbox').addClass('disabled');
+	});
+
+	if($.fn.selectpicker){
+		$('select:not(".without-styles")').selectpicker();
+	}
+}
+
 //Main Menu
 function mainMenu(){
 	var menu   = $('.main-menu'),
@@ -157,6 +185,41 @@ function animateFinish(){
 	});
 }
 
+
+
+
+//Slider Background
+function backgroundSlider() {
+	var slider = $('.background-slider');
+
+	if (slider.length) {
+		var autoplayTimeout = 2000,
+				animateIn       = 'fadeIn',
+				animateOut      = 'fadeOut';
+
+		if (slider.data('animateIn')) {
+			animateIn = slider.data('animateIn');
+		}
+
+		if (slider.data('animateOut')) {
+			animateOut = slider.data('animateOut');
+		}
+
+		if (slider.data('timeout')) {
+			autoplayTimeout = slider.data('timeout');
+		}
+
+		slider.owlCarousel({
+			animateIn       : animateIn,
+			animateOut      : animateOut,
+			items           : 1,
+			loop            : true,
+			autoplay        : true,
+			autoplayTimeout : autoplayTimeout
+		});
+	}
+}
+
 //Content Scroll
 function contentScroll(){
 	var section = $('.section.active');
@@ -217,6 +280,7 @@ $(document).ready(function(){
 
 	//Functions
 	mainMenu();
+	backgroundSlider();
 
 	//Functions(load)
 	$(window).on('load', function(){
