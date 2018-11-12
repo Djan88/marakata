17e006bbed2eae90fb7f5a06b221e65cc8e17463
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-11-12 18:58:16 +0300

    marakata_new-24

--- wp-content/themes/marakata_new/index.php
+++ wp-content/themes/marakata_new/index.php
@@ -650,7 +650,7 @@
 <script src="<?php bloginfo('template_url'); ?>/js/jquery.touchwipe.min.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/included-plagins.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/froogaloop2.min.js"></script>
-<!-- <script src="<?php //bloginfo('template_url'); ?>/js/main.js"></script> -->
+<script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/script.js"></script>
 <!-- Yandex.Metrika counter -->
 <script type="text/javascript">
