c31bc8f83b9ce88b5704c7bbef570d4849492173
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-03-25 20:20:01 +0300

    padding-top-15

--- wp-content/themes/marakata_new/js/script.js
+++ wp-content/themes/marakata_new/js/script.js
@@ -41,7 +41,7 @@ jQuery(function() {
           jQuery(".marakata_sim-wrap").css('height', scroll_val+'px');
           jQuery(".marakata_dot").css('height', scroll_val+'px');
       } else {
-          scroll_val =  block_w * 1.195377930902453;
+          scroll_val =  block_w * 1.195377930902472;
           jQuery(".marakata_sim-wrap").css('height', scroll_val+'px');
           jQuery(".marakata_dot").css('height', scroll_val+'px');
       }
